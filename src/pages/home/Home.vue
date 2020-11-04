fbd3b095f96d5e68c88db2df987641b477149a05
jimmydemx <j_demx@yahoo.com>
2020-11-04 20:13:56 +0100

    brach3

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,6 +2,7 @@
     <div>
         <home-header></home-header>
         <home-swiper></home-swiper>
+        <home-icons></home-icons>
       
     </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import HomeHeader from  './components/Header'
 import HomeSwiper from './components/swiper'
+import HomeIcons from './components/icons'
 export default {
     name: 'Home',
     components:{
         HomeHeader:HomeHeader,
         HomeSwiper: HomeSwiper,
+        HomeIcons:HomeIcons
     }
 
 }
